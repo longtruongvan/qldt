534c7108d90c7ad508027970a6eedc0d2ee023bf
longtv <longtv25102000@gmail.com>
2022-04-08 17:21:22 +0700

    Revert "no message"
    This reverts commit 7f341283

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widgets is the root of your application.
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
