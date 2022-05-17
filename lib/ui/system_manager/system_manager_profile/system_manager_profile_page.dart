065c6dd3dd689dec1ed7c170ccf39ffb8953632c
longtruongvan <102961077+longtruongvan@users.noreply.github.com>
2022-05-18 03:44:43 +0700

    update ui tab profile system manager

--- lib/ui/system_manager/system_manager_profile/system_manager_profile_page.dart
+++ lib/ui/system_manager/system_manager_profile/system_manager_profile_page.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:qldt/common/app_dimens.dart';
+import 'package:qldt/ui/auth/login/login_page.dart';
+import 'package:qldt/ui/widgets/button/app_button.dart';
+import 'package:qldt/utils/authentication.dart';
 
 class SystemManagerProfilePage extends StatefulWidget {
   const SystemManagerProfilePage({Key? key}) : super(key: key);
 
   @override
-  State<SystemManagerProfilePage> createState() => _SystemManagerProfilePageState();
+  State<SystemManagerProfilePage> createState() =>
+      _SystemManagerProfilePageState();
 }
 
 class _SystemManagerProfilePageState extends State<SystemManagerProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.amber,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            child: Column(
+              children: [],
+            ),
+          ),
+          Positioned(
+            child: AppButton(
+              title: 'Logout',
+              onPress: () {
+                Authentication.signOut();
+                Get.offAll(const LoginPage());
+              },
+            ),
+            bottom: AppDimens.spacingNormal,
+            right: AppDimens.spacingNormal,
+            left: AppDimens.spacingNormal,
+          )
+        ],
       ),
     );
   }
