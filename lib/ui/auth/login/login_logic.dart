902738100a7213f11e1b1332fe95b420be45011a
longtruongvan <102961077+longtruongvan@users.noreply.github.com>
2022-05-20 05:08:37 +0700

    add check first login

--- lib/ui/auth/login/login_logic.dart
+++ lib/ui/auth/login/login_logic.dart
@@ -6,6 +6,7 @@ import 'package:qldt/ui/auth/sign_up/sign_up_page.dart';
 import 'package:qldt/ui/home/home_page.dart';
 import 'package:qldt/utils/authentication.dart';
 
+import '../../../common/app_snack_bar.dart';
 import '../../../model/response/person_response.dart';
 import '../../../services/auth_service.dart';
 import '../../splash/splash_logic.dart';
@@ -43,21 +44,55 @@ class LoginLogic extends GetxController {
             Get.offAll(const StudentMainPage());
           } else if (response.type == PersonType.QTHT.name) {
             Get.offAll(const SystemManagerMainPage());
-          } else {
+          } else if (response.type == PersonType.GV.name) {
             Get.offAll(const TeacherMainPage());
           }
         } else {
-          Get.offAll(const LoginPage());
+          //checkFirstLogin
+          FirebaseFirestore.instance
+              .collection('Person')
+              .doc(uid)
+              .set(PersonResponse(
+              id: uid,
+              uid: uid,
+              name: user.displayName ?? '',
+              email: user.email ?? '',
+              phone: '',
+              type: PersonType.SV.name)
+              .toJson())
+              .then((value) {
+            FirebaseFirestore.instance
+                .collection('Person')
+                .doc(uid)
+                .get()
+                .then((value) {
+              if (value.data() != null) {
+                var response = PersonResponse.fromJson(value.data()!);
+                final service = Get.find<AuthService>();
+                service.updatePerson(response);
+                service.updateUser(user);
+                if (response.type == PersonType.SV.name) {
+                  Get.offAll(const StudentMainPage());
+                } else if (response.type == PersonType.QTHT.name) {
+                  Get.offAll(const SystemManagerMainPage());
+                } else if (response.type == PersonType.GV.name) {
+                  Get.offAll(const TeacherMainPage());
+                }
+              }
+            }).catchError((onError) {
+              AppSnackBar.showError(title: 'Error', message: 'An error');
+            });
+          }).catchError((onError) {});
         }
       }).catchError((onError) {
         Get.offAll(const LoginPage());
       });
-    }else{
+    } else {
       Get.offAll(const LoginPage());
     }
   }
 
-  void loginClickListener() async{
+  void loginClickListener() async {
     if (state.emailTextController.text.isEmpty) {
       state.emailValidate.value = false;
     } else {
--- lib/ui/splash/splash_logic.dart
+++ lib/ui/splash/splash_logic.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:qldt/common/app_snack_bar.dart';
 import 'package:qldt/model/response/person_response.dart';
 import 'package:qldt/services/auth_service.dart';
 import 'package:qldt/ui/auth/login/login_page.dart';
@@ -12,6 +13,7 @@ import 'package:qldt/ui/system_manager/main/system_manager_main_page.dart';
 import 'package:qldt/ui/teacher/teacher_main/teacher_main_page.dart';
 
 import '../../utils/authentication.dart';
+import '../student/student_register_subject/student_register_subject_page.dart';
 
 enum PersonType {
   SV,
@@ -21,7 +23,6 @@ enum PersonType {
 
 class SplashLogic extends GetxController {
   final state = SplashState();
-
 
   void checkLogin({required BuildContext context}) async {
     await Firebase.initializeApp();
@@ -45,16 +46,52 @@ class SplashLogic extends GetxController {
             Get.offAll(const StudentMainPage());
           } else if (response.type == PersonType.QTHT.name) {
             Get.offAll(const SystemManagerMainPage());
-          } else {
+          } else if (response.type == PersonType.GV.name) {
             Get.offAll(const TeacherMainPage());
+          } else {
+            Get.offAll(const StudentMainPage());
           }
         } else {
-          Get.offAll(const LoginPage());
+          //checkFirstLogin
+          FirebaseFirestore.instance
+              .collection('Person')
+              .doc(uid)
+              .set(PersonResponse(
+                      id: uid,
+                      uid: uid,
+                      name: user.displayName ?? '',
+                      email: user.email ?? '',
+                      phone: '',
+                      type: PersonType.SV.name)
+                  .toJson())
+              .then((value) {
+            FirebaseFirestore.instance
+                .collection('Person')
+                .doc(uid)
+                .get()
+                .then((value) {
+              if (value.data() != null) {
+                var response = PersonResponse.fromJson(value.data()!);
+                final service = Get.find<AuthService>();
+                service.updatePerson(response);
+                service.updateUser(user);
+                if (response.type == PersonType.SV.name) {
+                  Get.offAll(const StudentMainPage());
+                } else if (response.type == PersonType.QTHT.name) {
+                  Get.offAll(const SystemManagerMainPage());
+                } else if (response.type == PersonType.GV.name) {
+                  Get.offAll(const TeacherMainPage());
+                }
+              }
+            }).catchError((onError) {
+              AppSnackBar.showError(title: 'Error', message: 'An error');
+            });
+          }).catchError((onError) {});
         }
       }).catchError((onError) {
         Get.offAll(const LoginPage());
       });
-    }else{
+    } else {
       Get.offAll(const LoginPage());
     }
   }
